e4e85478fed90c289900676ed733464d4f6a21eb
leodavi3d <leodavi3d@hotmail.com>
2026-08-22 16:15:34 -0300

    feat: adiciona about panel e transicao de crossfade na tela de vitoria

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -1,5 +1,18 @@
 extends Control
 
+@onready var about_panel: Control = $AboutPanel
+@onready var BGVitoria: TextureRect = $BGVitoria
+@onready var escapou_label: Label = $EscapouLabel
+
+
+func _ready() -> void:
+	await get_tree().create_timer(5.0).timeout	
+	var tween: Tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(about_panel, "modulate:a", 1.0, 2.0)
+	tween.tween_property(BGVitoria, "modulate:a", 0.0, 2.0)
+	tween.tween_property(escapou_label, "modulate:a", 0.0, 2.0)
+
 func _unhandled_input(event: InputEvent) -> void:
-    if event.is_pressed() and event.is_action("ui_accept"):
-        get_tree().change_scene_to_file("res://scenes/splash.tscn")
+	if event.is_pressed() and event.is_action("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/splash.tscn")
